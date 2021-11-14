4623744c50f9e5252649984be3b9db2172d4e9fa
IanTaylor <76837421+iTaylorCodes@users.noreply.github.com>
2021-11-14 04:47:12 -0800

    Setup User Model

---
+++ helpers/sql.js
@@ -0,0 +1,19 @@
+/** Helper for making selective update SQL queries */
+
+const { BadRequestError } = require("../expressError");
+
+function sqlForPartialUpdate(dataToUpdate, jsToSql) {
+  const keys = Object.keys(dataToUpdate);
+  if (keys.length === 0) throw new BadRequestError("No data");
+
+  const cols = keys.map((colName, idx) => {
+    return `"${jsToSql[colName] || colName}"=$${idx + 1}`;
+  });
+
+  return {
+    setCols: cols.join(", "),
+    values: Object.values(dataToUpdate),
+  };
+}
+
+module.exports = { sqlForPartialUpdate };
