1f0b40199404a2e3ac86c792469676f213073546
IanTaylor <76837421+iTaylorCodes@users.noreply.github.com>
2021-11-14 02:11:58 -0800

    Setup JWT

---
+++ helpers/tokens.js
@@ -0,0 +1,14 @@
+/** return signed JWT from user data. */
+
+const jwt = require("jsonwebtoken");
+const { SECRET_KEY } = require("../config");
+
+function createToken(user) {
+  let payload = {
+    username: user.username,
+  };
+
+  return jwt.sign(payload, SECRET_KEY);
+}
+
+module.exports = { createToken };
