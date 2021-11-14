a198f6acde49cc75f64ebbe32607184b35a51372
IanTaylor <76837421+iTaylorCodes@users.noreply.github.com>
2021-11-13 22:54:12 -0800

    Setup Error Handling

--- neighborhood-schema.sql
+++ neighborhood-schema.sql
@@ -6,11 +6,11 @@ CREATE TABLE users (
   last_name TEXT NOT NULL,
   email TEXT NOT NULL
     CHECK (position('@' IN email) > 1)
-)
+);
 
 CREATE TABLE favorited_properties (
   user_id INTEGER
     REFERENCES users ON DELETE CASCADE,
   property_zpid INTEGER NOT NULL,
   PRIMARY KEY (user_id, property_zpid)
-)
+);
